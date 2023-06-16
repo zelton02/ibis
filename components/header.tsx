cd78a33ac099de699b80a6d39b306617d555d3bb
Lawrence3567 <fate3567@gmail.com>
2023-06-17 02:05:10 +0800

    Refined UI design and color codes
    color codes:
    #048AFF (blue)
    #B100E8 (purple)
    white
    #343541 (grey)
    #090909 (black)

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import { Notifications } from '@material-ui/icons';
 const Header = () => {
     return (
         <div className={style.header}>
-            <div className={style.title}>IBIS</div>
+            <div className={style.title}>iBIS</div>
             <div className={style.noti}>
                 <Notifications></Notifications>
             </div>
