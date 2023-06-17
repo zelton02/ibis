33d35cec332f58d9108366f2e13730d8d80ee398
“kks” <kkhai.shen1221@gmail.com>
2023-06-17 16:25:12 +0800

    home suggestion n alert

--- components/home/homeAlert.tsx
+++ components/home/homeAlert.tsx
@@ -3,21 +3,22 @@ import style from '@/styles/home/suggestion.module.css';
 import { Notifications } from '@material-ui/icons';
 
 
-const HomeSuggestion = () => {
+const HomeAlert = () => {
 
-    const suggestion: string[] = [
-        'Item B is trending in F&B. Suggest to add stock',
-        '40 Item E sold in 2 days. 85 left i inventory. Will sold out in 2 days. Suggest to add stock.',
-        'Supplier C may save 1.5% of cost for item G',
-        'Supplier D may save 8% of cost for item T',
+    const alert: string[] = [
+        'Item A left 25 in inventory, reaches lower threshold (30). Add stock asap.',
+        'Item B left 10 in inventory, reaches lower threshold (40). Add stock asap.',
+        '20 Item C expiried in 4 days ( 21 June 2023 ). Please take note.',
+        '250 Item C expiried in 12 days ( 29 June 2023 ). Please take note.',
+        
     ]
 
     return (
         <>
-            <h2>Suggestion</h2>
+            <h2>Alert</h2>
             <div className={style.table}>
                 {
-                    suggestion.map((s, index) => (
+                    alert.map((s, index) => (
                         <div className={index % 2 == 0 ? style.suggestionBar : style.suggestionBarr} key={index}>
                             {s}
                         </div>
@@ -29,4 +30,4 @@ const HomeSuggestion = () => {
     );
 };
 
-export default HomeSuggestion;
+export default HomeAlert;
