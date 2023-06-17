c697bbe2e24f19dad5c3ea48cc6a0217c3a948bd
“kks” <kkhai.shen1221@gmail.com>
2023-06-17 16:36:26 +0800

    item n suppler list

--- src/pages/item.tsx
+++ src/pages/item.tsx
@@ -1,18 +1,54 @@
 import Frame from "components/frame";
 import { naviPage } from "model/navi";
 import frame from '@/styles/Frame.module.css';
+import table from '@/styles/home/suggestion.module.css';
+import style from '@/styles/item/items.module.css';
+import { Item } from "model/data";
 
 
 export default function Item() {
+
+
+  const items: Item[] = [
+    {
+      item_id: 'ID',
+      item_name: 'Name',
+    },
+    {
+      item_id: '123',
+      item_name: 'asd',
+    },
+    {
+      item_id: '123',
+      item_name: 'asd',
+    },
+    {
+      item_id: '123',
+      item_name: 'asd',
+    },
+  ]
+
+
   return (
-    <div>
+    <>
 
       <Frame current={naviPage[1]} ></Frame>
       <div className={frame.frame}>
-      <h2>Item</h2>
+        <h2>Suggestion</h2>
+        <div className={table.table}>
+          {
+            items.map((i, index) => (
+              <div className={index % 2 == 0 ? table.suggestionBar : table.suggestionBarr} key={index}>
+                <div className={style.row}>
+                  <div className={style.id}> {i.item_id}</div>
+                  <div className={style.name}> {i.item_name}</div>
+                </div>
 
+              </div>
+            ))
+          }
+        </div>
       </div>
-
-    </div>
+    </>
   )
 }
--- src/pages/supplier.tsx
+++ src/pages/supplier.tsx
@@ -1,16 +1,73 @@
 import Frame from "components/frame";
 import { naviPage } from "model/navi";
 import frame from '@/styles/Frame.module.css';
-
+import { Supplier } from "model/data";
+import table from '@/styles/home/suggestion.module.css';
+import style from '@/styles/supplier/supplier.module.css';
 
 export default function Item() {
+
+  const suppliers: Supplier[] = [
+    {
+      supplier_id: 'ID',
+      supplier_name: 'Name',
+      supplier_contact: 'Contact',
+      supplier_email: 'Email',
+      supplier_location: 'Address',
+    },
+    {
+      supplier_id: '12',
+      supplier_name: 'QWERT SDN BHD',
+      supplier_contact: '1234567890',
+      supplier_email: 'qwert@asd.com',
+      supplier_location: 'Taman Beasr Yrah jhacbjhsbdc',
+    },
+    {
+      supplier_id: '12',
+      supplier_name: 'QWERT SDN BHD',
+      supplier_contact: '1234567890',
+      supplier_email: 'qwert@asd.com',
+      supplier_location: 'Taman Beasr Yrah jhacbjhsbdc',
+    },
+    {
+      supplier_id: '12',
+      supplier_name: 'QWERT SDN BHD',
+      supplier_contact: '1234567890',
+      supplier_email: 'qwert@asd.com',
+      supplier_location: 'Taman Beasr Yrah jhacbjhsbdc',
+    },
+    {
+      supplier_id: '12',
+      supplier_name: 'QWERT SDN BHD',
+      supplier_contact: '1234567890',
+      supplier_email: 'qwert@asd.com',
+      supplier_location: 'Taman Beasr Yrah jhacbjhsbdc',
+    },
+
+  ]
+
+
+
   return (
     <div>
 
       <Frame current={naviPage[2]} ></Frame>
       <div className={frame.frame}>
-      <h2>Supplier</h2>
+        <h2>Supplier</h2>
+        {
+          suppliers.map((s, index) => (
+            <div className={index % 2 == 0 ? table.suggestionBar : table.suggestionBarr} key={index}>
+              <div className={style.row}>
+                <div className={style.id}> {s.supplier_id}</div>
+                <div className={style.name}> {s.supplier_name}</div>
+                <div className={style.contact}> {s.supplier_contact}</div>
+                <div className={style.email}> {s.supplier_email}</div>
+                <div className={style.location}> {s.supplier_location}</div>
+              </div>
 
+            </div>
+          ))
+        }
       </div>
 
     </div>
